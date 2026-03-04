85c5021a7f1dfd1e57742e8578af08ab77260efa
AQUATECHIE <amolequwam96@gmail.com>
2026-03-04 15:21:40 +0100

    auto-commit

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -52,3 +52,40 @@ export const getAdminAnalytics = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+
+export const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find().select("-password");
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const updateUserRole = async (req, res) => {
+  const { role } = req.body;
+
+  const user = await User.findById(req.params.id);
+
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  user.role = role;
+  await user.save();
+
+  res.json({ message: "Role updated" });
+};
+
+export const deleteUser = async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  await user.deleteOne();
+
+  res.json({ message: "User deleted" });
+};
--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,9 +1,17 @@
 import express from "express";
 import { protect, adminOnly } from "../middleware/authMiddleware.js";
-import { getAdminAnalytics } from "../controllers/adminController.js";
+import { getAdminAnalytics,getAllUsers, updateUserRole, deleteUser } from "../controllers/adminController.js";
 
 const router = express.Router();
 
 router.get("/analytics", protect, adminOnly, getAdminAnalytics);
+// GET ALL USERS
+router.get("/users", protect, adminOnly, getAllUsers);
+
+// UPDATE USER ROLE
+router.put("/users/:id/role", protect, adminOnly, updateUserRole);
+
+// DELETE USER
+router.delete("/users/:id", protect, adminOnly, deleteUser);
 
 export default router;
